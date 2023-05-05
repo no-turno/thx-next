5e83305382f756b3aa264d6cb70b1f03b834e133
d-spindola <davispindola@proton.me>
2023-05-05 08:02:28 -0300

    feat: add global error handler

---
+++ app/global-error.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+// This is the new catch-all errors component
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+      </body>
+    </html>
+  )
+}
