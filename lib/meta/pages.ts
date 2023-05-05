71013e702258a53e1a89dc98d0b988aae2c13c5f
d-spindola <davispindola@proton.me>
2023-05-05 08:05:31 -0300

    feat: add folder for pages metadata

---
+++ lib/meta/pages.ts
@@ -0,0 +1,11 @@
+export const accountLayoutMetadata = {
+  title: "January/Account",
+  // TODO: add better description for each page
+  description: "",
+}
+
+export const signUpLayoutMetadata = {
+  title: "January/Register",
+  // TODO: add better description for each page
+  description: "",
+}
